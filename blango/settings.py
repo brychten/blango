8c573a16db89a70d659c57d44c636ab107ae8e44
Bernardo Rychtenberg <brychten@gmail.com>
2024-05-27 17:38:22 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -15,9 +15,14 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
 
+    REGISTRATION_OPEN = False
+
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve().parent.parent
 
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    
+    ACCOUNT_ACTIVATION_DAYS = 7
 
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
