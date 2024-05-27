745b0bffd9c3a6a173d068e3e6ff529c1c293a80
Bernardo Rychtenberg <brychten@gmail.com>
2024-05-27 14:11:04 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect 
 from django.utils import timezone
 from blog.models import Post
-from django.shortcuts import redirect
 from blog.forms import CommentForm
-import logging 
+import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 logger = logging.getLogger(__name__)
 
+# Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
