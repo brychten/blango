d616e1ef4186b0898a03616a8c9278d92bdcb8b4
Bernardo Rychtenberg <brychten@gmail.com>
2024-05-29 21:08:08 +0000

    Finish viewsets and routers"

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet, PostViewSet
 from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -18,8 +23,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    #path("posts/", PostList.as_view(), name="api_post_list"),
+    #path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
@@ -31,5 +36,10 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
+]
+
+urlpatterns = [
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 #urlpatterns = format_suffix_patterns(urlpatterns)
